15a947c05761a170d968f474c1f95a6a26d09df1
derek-corcoran-barrios <derek-corcoran-barrios@gmail.com>
2023-05-02 07:03:59 +0200

    Added logs to GetOccs, and skips repeated species

--- R/get_presence.R
+++ R/get_presence.R
@@ -7,6 +7,7 @@
 #' @param continent what contintent are the occurrences downloaded from
 #' @param country character, The 2-letter country code (ISO-3166-1) in which the occurrence was recorded.
 #' @param limit maximum number of occurrences downloaded
+#' @param Log Logical. If \code{TRUE}, a log file will be created with information on the progress of the function.
 #'
 #' @return If \code{WriteFile = TRUE}, this function does not return anything. If \code{WriteFile = FALSE}, a list containing the occurrence data for each species is returned.
 #'
@@ -20,7 +21,8 @@
 #' Presences <- SDMWorkflows::GetOccs(Species = c("Abies concolor", "Canis lupus"), WriteFile = FALSE)
 #' }
 
-GetOccs <- function(Species, WriteFile = T, continent = NULL, country = NULL, limit = 10000){
+GetOccs <- function(Species, WriteFile = T, continent = NULL, country = NULL, limit = 10000, Log = T){
+
   if (!is.character(Species)) {
     stop("Species argument must be a character vector")
   }
@@ -36,35 +38,73 @@ GetOccs <- function(Species, WriteFile = T, continent = NULL, country = NULL, li
   if (!is.numeric(limit)) {
     stop("Limit argument must be a numeric value")
   }
+
   if(WriteFile == T){
-    dir.create("Occs")
+    dir.create("Occs", showWarnings = FALSE)
   } else if(WriteFile == F){
     Presences <- list()
   }
+
+  if(Log == T){
+    log_file <- paste0("Occs/Log_", format(Sys.time(), "%Y-%m-%d_%H-%M-%S"), ".csv")
+    write.table(data.frame(Species = character(), Start_time = character(), End_time = character(), Error = character(), Num_presences = numeric()), file = log_file, row.names = FALSE, sep = ",")
+  }
+
   for(i in 1:length(Species)){
     message(paste("Starting species", i))
+
+    # Check if RDS file already exists
+    rds_file <- paste0("Occs/",janitor::make_clean_names(unique(Species[i])),".rds")
+    if (file.exists(rds_file)) {
+      message(paste("Skipping", Species[i], "- RDS file already exists"))
+      if(Log == T){
+        log_entry <- data.frame(Species = Species[i], Start_time = NA, End_time = NA, Error = "RDS file exists", Num_presences = NA)
+        write.table(log_entry, file = log_file, append = TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
+      }
+      next
+    }
+
+    # Download occurrence data
     Occs <- tryCatch(
-      expr = {rgbif::occ_data(scientificName = Species[i],
-                                 hasCoordinate = T,
-                                 continent = continent,
-                                 country = country,
-                                 hasGeospatialIssue=FALSE,
-                                 limit = limit)},
+      expr = {
+        rgbif::occ_data(scientificName = Species[i],
+                        hasCoordinate = T,
+                        continent = continent,
+                        country = country,
+                        hasGeospatialIssue = FALSE,
+                        limit = limit)
+      },
       error = function(e) {
         message(paste("Error for species", Species[i], Sys.time()))
+        if(Log == T){
+          log_entry <- data.frame(Species = Species[i], Start_time = NA, End_time = format(Sys.time()), Error = "Error downloading occurrence data", Num_presences = NA)
+          write.table(log_entry, file = log_file, append = TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
+        }
         NULL
       })
-    message(paste(i, "of", length(Species), "ready!", Sys.time()))
-    if(WriteFile == TRUE){
-      try({saveRDS(Occs$data, paste0("Occs/",janitor::make_clean_names(unique(Species[i])),".rds"))})
-    }else if(WriteFile == FALSE){
-      Presences[[i]] <- Occs$data
+
+    # Save occurrence data or add to list
+    if (is.null(Occs)) {
+      message(paste("Skipping", Species[i], "- Error downloading occurrence data"))
+    } else {
+      if(WriteFile == T){
+        try({saveRDS(Occs$data, rds_file)})
+      } else if(WriteFile == F){
+        Presences[[i]] <- Occs$data
+      }
+      if(Log == T){
+        log_entry <- data.frame(Species = Species[i], Start_time = format(Sys.time()), End_time = format(Sys.time()), Error = NA, Num_presences = nrow(Occs$data))
+        write.table(log_entry, file = log_file, append = TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
+      }
     }
+
     rm(Occs)
     gc()
+
+    message(paste(i, "of", length(Species), "ready!", Sys.time()))
   }
+
   if(WriteFile == F){
     return(Presences)
   }
 }
-
